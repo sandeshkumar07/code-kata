8dbeed297df532997efecaf0d34d66152f1525a5
Sandesh Kumar <sandesh@moven.com>
2016-03-24 14:55:36 -0400

    Initial commit;hacked my way through TDD

--- src/kata.js
+++ src/kata.js
@@ -1,3 +1,14 @@
-function Person() {
-  return { greet: "Hello!" };
+function CheckForUniqueChars (string) {
+  var hashmap = {};
+  if(string) {
+    for (var i = 0; i < string.length; i++) {
+      if (hashmap[string[i]] != null) {
+        hashmap[string[i]] = 1;
+        return {result: false};
+      } else {
+        hashmap[string[i]] = 0;
+      }
+    }
+  }
+  return {result: true};
 }
--- test/kata_spec.js
+++ test/kata_spec.js
@@ -1,6 +1,22 @@
-describe("Person", function () {
-  it("has a greeting", function() {
-    var person = new Person();
-    expect(person.greet).toBe("Hello!");
+describe("Check unique characters", function () {
+  it("if string with duplicate", function() {
+    var check = new CheckForUniqueChars('Hi mom');
+    expect(check.result).toBe(false);
+  });
+  it("if string with duplicate but different cases", function() {
+    var check = new CheckForUniqueChars('Hi Mom');
+    expect(check.result).toBe(true);
+  });
+  it("if no duplicate", function() {
+    var check = new CheckForUniqueChars('Hey pa');
+    expect(check.result).toBe(true);
+  });
+  it("if empty string", function() {
+    var check = new CheckForUniqueChars('');
+    expect(check.result).toBe(true);
+  });
+  it("if undefined", function() {
+    var check = new CheckForUniqueChars();
+    expect(check.result).toBe(true);
   });
 });
