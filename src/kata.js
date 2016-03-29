7a94858fd05d0ce4c041a54d2ed38bd8325f8ad1
Sandesh Kumar <sandesh@moven.com>
2016-03-28 23:18:20 -0400

    Removed log statements

--- src/kata.js
+++ src/kata.js
@@ -13,22 +13,14 @@ function CheckForUniqueChars (string) {
   //return {result: true};
   var bitMap = 0;
   if(string) {
-    console.log(string)
     string = string.replace(/\s+/g, '');
-    console.log(string)
-    console.log("BitMap", (bitMap >>> 0).toString(2));
     for (var i = 0; i < string.length; i++) {
       var charKey = (1 << string.charCodeAt(i));
-      console.log("Bit posn val ", ((charKey)>>>0).toString(2) + " AND with bitMap "+  ((bitMap & charKey) >>> 0).toString(2));
-      console.log("Bit posn val ", charKey + " AND with bitMap "+  ((bitMap & charKey)));
       if ((bitMap & charKey) === charKey) {
-        console.log('here')
         bitMap |= (0 << string.charCodeAt(i));
         return {result: false};
       } else {
-        console.log("Adding character ", string[i]);
         bitMap |= charKey;
-        console.log("After adding character bitMap", (bitMap >>> 0).toString(2));
       }
     }
   }
