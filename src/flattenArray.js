abf5e9b7581692981a5033afa660fc48eb3fd3ed
Sandesh Kumar <sandesh@moven.com>
2016-04-05 13:28:30 -0400

    Refactoring the return

--- src/flattenArray.js
+++ src/flattenArray.js
@@ -12,9 +12,10 @@ function flatRecursive(arr, arr2) {
     var flattenedArr = arr2;
     for( var i = 0;  i < arr.length; i++) {        
         var el = arr[i];
-        var items = Array.isArray(el) ? flatRecursive(el, flattenedArr): el;
         if(!Array.isArray(el)) {
             flattenedArr.push(el);
+        } else {
+            flatRecursive(el, flattenedArr)
         }
     }
     return flattenedArr;
