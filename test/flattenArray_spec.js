1eaac5947ee460a25a129e95c7d8c8bbb3a3f69b
Sandesh Kumar <sandesh@moven.com>
2016-04-06 10:24:44 -0400

    Updated the flattenArray_spec file

--- test/flattenArray_spec.js
+++ test/flattenArray_spec.js
@@ -4,14 +4,14 @@
 describe("Flatten multi dimensional arrays", function () {
     it("if array has many arrays", function () {
         var res = flattenArray([1, [2, 3], 4, [5, 6, 7]]);
-        //expect(res).toEqual([1, 2, 3, 4 , 5, 6, 7]);
+        expect(res).toEqual([1, 2, 3, 4 , 5, 6, 7]);
     });
-    //it("if array is a single array", function () {
-    //    var res = flattenArray([1, 2, 3]);
-    //    //expect(res).toEqual([1, 2, 3]);
-    //});
-    //it("if array has many arrays", function () {
-    //    var res = flattenArray(['a', 'b', ['c', 'd'], ['e',['f','g']], 'h', 'i' , ['j','k','l','m']]);
-    //    //expect(res).toEqual(['a','b','c','d','e','f','g','h','i','j','k','l','m']);
-    //});
+    it("if array is a single array", function () {
+        var res = flattenArray([1, 2, 3]);
+        expect(res).toEqual([1, 2, 3]);
+    });
+    it("if array has many arrays", function () {
+        var res = flattenArray(['a', 'b', ['c', 'd'], ['e',['f','g']], 'h', 'i' , ['j','k','l','m']]);
+        expect(res).toEqual(['a','b','c','d','e','f','g','h','i','j','k','l','m']);
+    });
 });
